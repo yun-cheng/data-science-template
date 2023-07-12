dd56789f8e89362a675d3412927f6e4759f6825a
yun-cheng <yuncheng.dev@gmail.com>
2023-07-12 09:16:52 +0800

    update common.R

--- src/common/common.R
+++ src/common/common.R
@@ -1,12 +1,15 @@
-library(data.table)
 library(magrittr)
 library(stringr)
 library(stringi)
 library(fst)
+library(lubridate)
 library(glue)
 library(readxl)
+library(data.table) # * 放後面才能覆蓋前面套件的 functions
 
 transformers <- tidyverse_style()
 transformers$token$fix_quotes <- NULL
 
 styler:::set_style_transformers()
+
+# DATE <- today() %>% str_remove_all('-') %>% str_sub(3)
